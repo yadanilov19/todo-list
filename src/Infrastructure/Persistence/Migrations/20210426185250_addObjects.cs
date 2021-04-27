946b67c7f005d7b515e2ef6b021f1dbfc69a5c0d
yadanilov19 <yadanilov@gmail.com>
2021-04-27 12:54:21 +0500

    Забытые файлы миграции

---
+++ src/Infrastructure/Persistence/Migrations/20210426185250_addObjects.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArchitecture.Infrastructure.Persistence.Migrations
+{
+    public partial class addObjects : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Reminder",
+                table: "TodoItems",
+                newName: "ExpiryDate");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TodoItemRefId",
+                table: "TodoItems",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TodoItems_TodoItemRefId",
+                table: "TodoItems",
+                column: "TodoItemRefId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TodoItems_TodoItems_TodoItemRefId",
+                table: "TodoItems",
+                column: "TodoItemRefId",
+                principalTable: "TodoItems",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TodoItems_TodoItems_TodoItemRefId",
+                table: "TodoItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TodoItems_TodoItemRefId",
+                table: "TodoItems");
+
+            migrationBuilder.DropColumn(
+                name: "TodoItemRefId",
+                table: "TodoItems");
+
+            migrationBuilder.RenameColumn(
+                name: "ExpiryDate",
+                table: "TodoItems",
+                newName: "Reminder");
+        }
+    }
+}
